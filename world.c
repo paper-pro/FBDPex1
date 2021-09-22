2f767fcc5a961262ba421befb7ebc1325f8e22d1
paper-pro <75004047+paper-pro@users.noreply.github.com>
2021-09-22 23:30:24 +0800

    assorted changes

--- world.c
+++ world.c
@@ -1,4 +1,4 @@
-#define N 100000000
+#define N 1000000
 #define a 10
 #define b 100
 #include <stdio.h>
@@ -16,11 +16,15 @@ MPI_Init(&argc, &argv);
 MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 MPI_Comm_size(MPI_COMM_WORLD,&numprocs);--numprocs;
 if (myid== 0){
+clock_t start,end;
+start = clock();
 for (int i = 0; i < N; ++i )
 { x = a+i*dx+dx/2;
 MPI_Send(&x, 1, MPI_DOUBLE, i%numprocs+1, 1 ,MPI_COMM_WORLD);}
 for (int source = 1; source <= numprocs; ++source)
 { MPI_Recv(&local, 1, MPI_DOUBLE, source, 99, MPI_COMM_WORLD, &status);inte += local ; }
+end = clock();
+printf("time=%f\n",(double)(end-start)/CLOCKS_PER_SEC);
 }else
 { for (i = myid-1; i < N; i=i+numprocs)
 { MPI_Recv(&local, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status); inte+= local*local*local*dx; }
